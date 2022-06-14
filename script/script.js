e253e91f07be6978b88848a47669f4f8d155eec3
pyroYV <simonerizzo.pvt@gmail.com>
2022-06-14 14:32:22 +0200

    con i check

--- script/script.js
+++ script/script.js
@@ -1,18 +1,19 @@
-const priceKm = 0.21
+const priceKm = 0.21;
 
-const userKm = prompt('Enter how long is your trip in Km')
-const userAge = parseInt(prompt ('How old are you?'))
+const userKm = prompt('Enter how long is your trip in Km');
+const userAge = parseInt(prompt ('How old are you?'));
+let standardPrice = userKm * priceKm;
 
-console.log(userKm,userAge)
-
-let standardPrice = userKm * priceKm
-console.log(standardPrice)
-if (userAge < 18){
-    let discounted = (standardPrice * 80 / 100)
-    console.log(discounted.toFixed(2))
-} else if(userAge > 60){
-    let discounted = standardPrice * 60 / 100
-    console.log(discounted.toFixed(2))
+if ((isNaN(userKm) == true)  || (isNaN(userAge) == true) ||  (userKm < 0) || (userAge < 0)){
+    console.log("error, only positive numbers")
 } else {
-    console.log(standardPrice)
+    if (userAge < 18){
+        let discounted = (standardPrice * 80 / 100)
+        console.log("Your ticket's price was " + standardPrice.toFixed(2), "but since you're really young you'll pay " + discounted.toFixed(2));
+    } else if(userAge > 65){
+        let discounted = standardPrice * 60 / 100
+        console.log("Your price was " + standardPrice.toFixed(2), "but since you're really old you'll pay " + discounted.toFixed(2))
+    } else {
+        console.log("Your ticket's price is" + standardPrice.toFixed(2))
+    }
 }
